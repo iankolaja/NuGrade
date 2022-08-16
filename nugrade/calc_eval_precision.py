89d2539e2700634ff5a867d98b392cec0f587e05
iankolaja <iankolaja@gmail.com>
2022-08-15 17:55:38 -0700

    interpolation performed in this function now

--- nugrade/calc_eval_precision.py
+++ nugrade/calc_eval_precision.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 def calc_eval_precision(exfor_dataset, evaluation_energy, evaluation_xs, lower_energy, upper_energy):
+    # Convert dataframe to numpy and filter out data out of energy range
     exfor_energy = exfor_dataset['Energy'].to_numpy()
     exfor_xs = exfor_dataset['Data'].to_numpy()
     filtered_booleans = np.where(np.logical_and(exfor_energy >= lower_energy, exfor_energy <= upper_energy))
@@ -9,10 +10,8 @@ def calc_eval_precision(exfor_dataset, evaluation_energy, evaluation_xs, lower_e
     exfor_xs = exfor_xs[filtered_booleans]
     if len(exfor_energy) == 0:
         return None
-    _, unique_indices = np.unique(exfor_energy, return_index=True)
-    exfor_energy = exfor_energy[unique_indices]
-    exfor_xs = exfor_xs[unique_indices]
-    _, _, evaluation_indices = np.intersect1d(exfor_energy, evaluation_energy, return_indices=True)
-    standard_deviation = np.sqrt(np.sum(np.square(exfor_xs - evaluation_xs[evaluation_indices])) /
-                                 len(evaluation_indices))
-    return standard_deviation
+    # Interpolate evaluation cross sections at the exfor energy points
+    interpolated_xs = np.interp(exfor_energy, evaluation_energy, evaluation_xs)
+    # Calculate average absolute relative error
+    average_absolute_relative_error = np.sum(np.abs(exfor_xs - interpolated_xs)/exfor_xs)/len(exfor_energy)*100
+    return average_absolute_relative_error
