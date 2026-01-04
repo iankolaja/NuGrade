7326df3c5cd59a7d35cb42046d487eb32017e091
Ian Kolaja <iankolaja@gmail.com>
2026-01-03 22:58:08 -0800

    Updated handpicked results

--- tests/test_database_metrics.py
+++ tests/test_database_metrics.py
@@ -5,11 +5,25 @@ import unittest
 class TestDatabaseParsing(unittest.TestCase):
     def test_database_parsing(self):
         options = MetricOptions()
-        options.set_slow_neutrons()
+        options.set_neutrons()
         test_isotope = "7Li"
-        test_metric = grade_isotope(exfor_data, test_isotope, options)
-        self.assertEqual(test_metric.num_experiments, 140)
-        self.assertEqual(test_metric.SIG_measurements['(N,TOT)'].num_measurements, 41)
+        test_Z = 3
+        test_A = 7
+        test_nuclide_metrics = grade_isotope(test_Z, test_A, test_isotope, options)
+
+        # Verify that all expected datasets were found
+        found_num_datasets = test_nuclide_metrics.num_datasets
+        expected_num_datasets = 17
+        self.assertEqual(found_num_datasets, expected_num_datasets)
+
+        found_ntot_measurements = test_nuclide_metrics.reactions['N,TOT'].num_measurements
+        expected_ntot_measurements = 17
+        self.assertEqual(found_ntot_measurements, expected_ntot_measurements)
+
+        # Verify that all expected data points are found
+        found_ntot_datapoints = test_nuclide_metrics.reactions['N,TOT'].num_datapoints
+        expected_ntot_datapoints = 8107
+        self.assertEqual(found_ntot_datapoints, expected_ntot_datapoints)
 
 
 if __name__ == '__main__':
