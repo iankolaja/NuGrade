1692660eed0b7e451d37faa9988612090cd66211
Ian Kolaja <iankolaja@gmail.com>
2026-01-03 22:59:59 -0800

    updated to include linear and log scale

--- nugrade/calc_energy_coverage.py
+++ nugrade/calc_energy_coverage.py
@@ -1,37 +1,32 @@
 import numpy as np
 
 
-def calc_energy_coverage(sorted_energy_array, lower_energy, upper_energy, energy_width):
-    sorted_energy_array = np.insert(sorted_energy_array, 0, lower_energy)
-    sorted_energy_array = np.append(sorted_energy_array, upper_energy)
-    total_energy_space = upper_energy - lower_energy
-    energy_covered = 0.0
-    for i in range(1, len(sorted_energy_array) - 1):
-        # Calculate left side
-        # If the last datapoint is within an energy width, calculate the distance between them
-        # print("energy: {0}".format(sorted_energy_array[i]))
-        if sorted_energy_array[i] - energy_width / 2.0 < sorted_energy_array[i - 1]:
-            # print("branch 1")
-            energy_covered += sorted_energy_array[i] - sorted_energy_array[i - 1]
-            # print(energy_covered)
-        else:
-            # print("branch 2")
-            energy_covered += energy_width / 2.0
-            # print(energy_covered)
 
-        # Calculate right side
-        if sorted_energy_array[i] + energy_width <= sorted_energy_array[i + 1]:
-            # print("branch 3")
-            energy_covered += energy_width / 2.0
-            # print(energy_covered)
-        elif sorted_energy_array[i] + energy_width / 2 <= sorted_energy_array[i + 1]:
-            # print("branch 4")
-            energy_covered += sorted_energy_array[i + 1] - sorted_energy_array[i] - energy_width / 2.0
-            # print(energy_covered)
-        else:
-            # print("branch 5")
-            pass
-            # print(energy_covered)
-    if sorted_energy_array[-2] + energy_width / 2.0 > sorted_energy_array[-1]:
-        energy_covered += sorted_energy_array[-1] - sorted_energy_array[-2]
-    return round(energy_covered / total_energy_space * 100, 3)
+def calc_energy_coverage(channel_data, options):
+    lower_energy = options.lower_energy
+    upper_energy = options.upper_energy
+    energy_width = options.energy_width
+    scale = options.energy_coverage_scale
+    energies = channel_data['Energy'].to_numpy()
+
+    if scale == "log":
+        energies = np.log10(channel_data['Energy'].to_numpy())
+        lower_energy = np.log10(lower_energy)
+        upper_energy = np.log10(upper_energy)
+
+    elif scale == "linear":
+        pass
+
+    else:
+        raise Exception(f"Unknown energy coverage scale option: {scale}")
+
+    assert_message = "calc_energy_coverage failed, energies not ascending."
+    assert np.all(energies[:-1] <= energies[1:]), assert_message
+
+    energies = np.hstack((lower_energy, energies, upper_energy))
+    total_energy_space = upper_energy - lower_energy
+    differences = np.diff(energies)
+    differences[0] += energy_width/2
+    differences[-1] += energy_width/2
+    unoccupied_space = np.sum(differences[differences>energy_width]-energy_width)
+    return round((total_energy_space-unoccupied_space)/total_energy_space*100,7)
