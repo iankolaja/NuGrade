5db960c57ba983993dba0750abcafe8af648c433
Ian Kolaja <iankolaja@gmail.com>
2026-01-03 23:00:20 -0800

    Cut down on number of importable functions

--- nugrade/__init__.py
+++ nugrade/__init__.py
@@ -1,17 +1,6 @@
 from .nuclide import Nuclide, Reaction
 from .metric_options import MetricOptions
-from .grading_functions import grade_isotope, grade_many_isotopes, plot_grades, plot_precision_data
-import pandas as pd
+from .grading_functions import grade_isotope, grade_many_isotopes, plot_grades
 
-try:
-    import importlib.resources as pkg_resources
-except ImportError:
-    # Try backported to PY<37 `importlib_resources`.
-    import importlib_resources as pkg_resources
-
-pickle_path = pkg_resources.open_binary('nugrade', 'EXFOR.pkl')
-exfor_data = pd.read_pickle(pickle_path, compression=None)
-
-__all__ = ['Nuclide', 'Reaction', 'exfor_data', 'MetricOptions',
-           'grade_isotope', 'grade_many_isotopes', 'plot_grades',
-           'plot_precision_data']
+__all__ = ['Nuclide', 'Reaction', 'MetricOptions',
+           'grade_isotope', 'grade_many_isotopes', 'plot_grades']
