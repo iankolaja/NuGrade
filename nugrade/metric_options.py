90651c27845ea7a333553b780fb55420bede1b92
Ian Kolaja <iankolaja@gmail.com>
2026-02-07 17:28:06 -0800

    Removed flag for user defined

--- nugrade/metric_options.py
+++ nugrade/metric_options.py
@@ -1,17 +1,17 @@
+#TODO: Delete "Scored channel"
 class MetricOptions:
     def __init__(self):
-        self.lower_energy = None  # eV Lower bound for spectral calculations
-        self.upper_energy = None  # eV Upper bound for spectral calculations
-        self.energy_width = None  # eV Width associated with data points for energy completeness
-        self.evaluation = None  # Evaluation to be used for precision calculation
-        self.required_reaction_channels = [(1, 'N,TOT'), ]  # What reaction channels should be measured for the application
-        self.scored_channel = 'N,TOT'
-        self.scored_metric = "relative_error"
-        self.projectile = "n"
+        self.lower_energy = 0.0  # eV Lower bound for spectral calculations
+        self.upper_energy = 0.0  # eV Upper bound for spectral calculations
+        self.energy_width = 0.0  # eV Width associated with data points for energy completeness
+        self.evaluation = ""  # Evaluation to be used for precision calculation
+        self.required_reaction_channels = []  # What reaction channels should be measured for the application
+        self.scored_channel = ""
+        self.scored_metric = ""
+        self.projectile = ""
         self.energy_coverage_scale = "linear"
         self.user_defined = False
         self.weighting_function = None
-        self.mode = None
 
     def to_dict(self):
         opt_dict = dict()
@@ -22,23 +22,22 @@ class MetricOptions:
         opt_dict['evaluation'] = self.evaluation
         opt_dict['required_reaction_channels'] = self.required_reaction_channels
         opt_dict['user_defined'] = self.user_defined
-        opt_dict['mode'] = self.mode
         opt_dict['scored_metric'] = self.scored_metric
         opt_dict['weighting_function'] = self.weighting_function
         return opt_dict
 
     def set_neutrons(self):
-        self.lower_energy = 0.01
+        self.lower_energy = 1.0E-08
         self.upper_energy = 5.0E6
         self.energy_coverage_scale = "linear"
         self.energy_width = 0.01
         self.evaluation = "endf8"
+        self.scored_channel = 'N,TOT'
         self.required_reaction_channels = [(1, 'N,TOT')]#, (2, 'N,EL')]#, (3 'N,INL'), (102, 'N,G')]
         self.projectile = "n"
         self.user_defined = False
         self.scored_metric = "chi_squared"
         self.weighting_function = None
-        self.mode = "reactor_physics"
 
 
     def set_protons(self):
@@ -53,6 +52,5 @@ class MetricOptions:
         self.user_defined = False
         self.weighting_function = None
         self.scored_metric = "chi_squared"
-        self.mode = "reactor_physics"
 
 
