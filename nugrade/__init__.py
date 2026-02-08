9e5d29f0e9b7263f65587fea120006d7f40bcb85
Ian Kolaja <iankolaja@gmail.com>
2026-02-08 15:35:56 -0800

    added new functions

--- nugrade/__init__.py
+++ nugrade/__init__.py
@@ -1,6 +1,8 @@
 from .nuclide import Nuclide, Reaction
 from .metric_options import MetricOptions
-from .grading_functions import grade_isotope, grade_many_isotopes, plot_grades
+from .grading_functions import grade_isotope, grade_many_isotopes, plot_grades, nuclide_symbol_format
+from .ai_agent import NuclearDataAgent
 
 __all__ = ['Nuclide', 'Reaction', 'MetricOptions',
-           'grade_isotope', 'grade_many_isotopes', 'plot_grades']
+           'grade_isotope', 'grade_many_isotopes', 'plot_grades',
+           'nuclide_symbol_format']
