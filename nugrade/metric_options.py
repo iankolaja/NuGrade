85024f51bc5511557e13359e24adf53c992c5468
Ian Kolaja <iankolaja@gmail.com>
2026-02-09 02:42:59 -0800

    made log default value, got rid of scored metric, added function for printing metric as text

--- nugrade/metric_options.py
+++ nugrade/metric_options.py
@@ -7,10 +7,9 @@ class MetricOptions:
         self.energy_width = 0.0  # eV Width associated with data points for energy completeness
         self.evaluation = ""  # Evaluation to be used for precision calculation
         self.required_reaction_channels = []  # What reaction channels should be measured for the application
-        self.scored_channel = ""
         self.scored_metric = ""
         self.projectile = ""
-        self.energy_coverage_scale = "linear"
+        self.energy_coverage_scale = "log"
         self.weighting_function = None
 
     def to_dict(self):
@@ -25,16 +24,21 @@ class MetricOptions:
         opt_dict['weighting_function'] = self.weighting_function
         return opt_dict
     
+    def get_metric_text(self):
+        metric_text_dict = {"chi_squared": "Chi Squared",
+                          "relative_error": "Absolute Relative Error (%)"}
+        return metric_text_dict[self.scored_metric]
+    
     def gen_html_description(self, do_html=True):
         reactions_str = "("+self.required_reaction_channels[0][1]+")"
         for reaction in self.required_reaction_channels[1:]:
             reactions_str += f", ({reaction[1]})"
 
         metric_text_dict = {"chi_squared": "Chi Squared",
-                          "reltaive_error": "Relative Error"}
+                          "relative_error": "Absolute Relative Error"}
         metrics_str = metric_text_dict[self.scored_metric]
-        lower_energy_str = np.format_float_scientific(self.lower_energy, precision=4,exp_digits=1)
-        upper_energy_str = np.format_float_scientific(self.upper_energy, precision=4,exp_digits=1)
+        lower_energy_str = np.format_float_scientific(self.lower_energy, precision=4,exp_digits=1,unique=True, min_digits=2)
+        upper_energy_str = np.format_float_scientific(self.upper_energy, precision=4,exp_digits=1,unique=True, min_digits=2)
         if do_html:
             param_text = f"""
             <p class=\"options-report-text\"><b>Scoring Parameters</b><br>
@@ -56,12 +60,11 @@ class MetricOptions:
     def set_neutrons(self):
         self.lower_energy = 0.01
         self.upper_energy = 5.0E6
-        self.energy_width = 100
-        self.energy_coverage_scale = "linear"
+        self.energy_width = 0.01
+        self.energy_coverage_scale = "log"
         self.evaluation = "endf8"
         self.projectile = "n"
-        self.required_reaction_channels = [(1, 'N,TOT')]#, (2, 'N,EL')]#, (3 'N,INL'), (102, 'N,G')]
-        self.scored_channel = 'N,TOT'
+        self.required_reaction_channels = [(1, 'N,TOT'), (2, 'N,EL'), (3, 'N,INL'), (102, 'N,G')]
         self.scored_metric = "chi_squared"
         self.weighting_function = None
 
@@ -69,12 +72,11 @@ class MetricOptions:
     def set_protons(self):
         self.lower_energy = 0.01
         self.upper_energy = 5.0E6
-        self.energy_width = 100
-        self.energy_coverage_scale = "linear"
+        self.energy_width = 0.01
+        self.energy_coverage_scale = "log"
         self.evaluation = "endf8"
         self.projectile = "p"
         self.required_reaction_channels = [(2, 'P,EL'), (3, 'P,INL')]
-        self.scored_channel = 'P,EL'
         self.weighting_function = None
         self.scored_metric = "chi_squared"
 
