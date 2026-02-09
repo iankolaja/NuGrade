486fe434593ecae10cd8fd4d55d31c03d4f39d9c
Ian Kolaja <iankolaja@gmail.com>
2026-02-09 02:39:34 -0800

    fixed some state and form i/o

--- startnugrade.py
+++ startnugrade.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request
+from flask import request, session
 from flask import render_template
 from nugrade import *
 import pandas as pd
@@ -18,6 +18,7 @@ md = MarkdownIt(
         "typographer": True,
     }
 )
+
 version = '0.0.1'
 options = MetricOptions()
 options.set_neutrons()
@@ -50,10 +51,11 @@ else:
 
 if ai_available:
     chat_history = []
-    response, chat_history = claude_agent.chat("How good is the 35Cl cross section for MSFR design work?",conversation_history=chat_history,metrics=metrics, options=options)
-    print(response)
-    ai_chat_history = "<p class=\"user-message\"> How good is the 35Cl cross section for MSFR design work?</p>"
-    formatted_response = md.render(response)
+    #response, chat_history = claude_agent.chat("How good is the 35Cl cross section for MSFR design work?",conversation_history=chat_history,metrics=metrics, options=options)
+    #print(response)
+    ai_chat_history = "<div class=\"user-message-bubble\"><p class=\"user-message\"> How good is the 35Cl cross section for MSFR design work?</p></div>"
+    #formatted_response = md.render(response)
+    formatted_response = md.render("Here is some exmaple response text.")
     ai_chat_history += f"<p class=\"agent-message\"> {formatted_response}</p>"
 
 
@@ -75,20 +77,20 @@ def render_for_particle(particle, options, version, text_report, plot_script, pl
 
 @app.route('/')
 def index():
+    plot_script, plot_component = plot_grades(metrics, options)
+    options_text = options.gen_html_description()
     return render_for_particle(options.projectile, options, version,
                                text_report, plot_script, plot_component, ai_chat_history, options_text)
 
 
-
-
-@app.route('/generate_neutrons', methods=['POST'])
-def generate_neutrons():
+def process_base_form():
     options.lower_energy = float(request.form['lower_energy'])
     options.upper_energy = float(request.form['upper_energy'])
+    print(request.form.get('energy_coverage_scale', False))
     if request.form.get('energy_coverage_scale', False):
-        options.energy_coverage_scale = "linear"
-    else:
         options.energy_coverage_scale = "log"
+    else:
+        options.energy_coverage_scale = "linear"
     options.energy_width = float(request.form['energy_width'])
 
     try:
@@ -96,9 +98,9 @@ def generate_neutrons():
     except:
         print("Failed to access form evaluation-lib")
         evaluation_response = None
-    if evaluation_response == 1 or evaluation_response is None:
+    if evaluation_response == "1" or evaluation_response is None:
         options.evaluation = "endf8"
-    if evaluation_response == 2:
+    if evaluation_response == "2":
         options.evaluation = "endf7-1"
 
     try:
@@ -106,10 +108,10 @@ def generate_neutrons():
     except:
         print("Failed to access form scored-metric")
         scored_response = None
-    if scored_response == 1 or scored_response is None:
+    if scored_response == "1" or scored_response is None:
         options.scored_metric = "chi_squared"
-    if scored_response == 2:
-        options.evaluation = "relative_error"
+    if scored_response == "2":
+        options.scored_metric = "relative_error"
 
     try:
         weighting_response = request.form['weighting-function']
@@ -117,15 +119,18 @@ def generate_neutrons():
         print("Failed to access form weighting-function")
         weighting_response = None
 
-    if weighting_response == 1 or weighting_response is None:
+    if weighting_response == "1" or weighting_response is None:
         options.weighting_function = None
-    if weighting_response == 2:
+    if weighting_response == "2":
         options.weighting_function = "maxwell-boltzmann-room-temp"
-    if weighting_response == 3:
+    if weighting_response == "3":
         options.weighting_function = "maxwell-boltzmann-320C"
-    if weighting_response == 4:
+    if weighting_response == "4":
         options.weighting_function = "watt"
 
+@app.route('/generate_neutrons', methods=['POST'])
+def generate_neutrons():
+    process_base_form()
     options.required_reaction_channels = []
     if request.form.get('n,tot', False):
         options.required_reaction_channels += [(1, 'N,TOT')]
@@ -135,60 +140,16 @@ def generate_neutrons():
         options.required_reaction_channels += [(3, 'N,INL')]
     if request.form.get('n,g', False):
         options.required_reaction_channels += [(102, 'N,G')]
-    #if bool(request.form['n,p']):
-    #    options.required_reaction_channels += [(103, 'N,P')]
 
     metrics = grade_many_isotopes(options)
     plot_script, plot_component = plot_grades(metrics, options)
+    options_text = options.gen_html_description()
     return render_for_particle(options.projectile, options, version,
                                text_report, plot_script, plot_component, ai_chat_history, options_text)
 
 
 @app.route('/generate_protons', methods=['POST'])
 def generate_protons():
-    options.lower_energy = float(request.form['lower_energy'])
-    options.upper_energy = float(request.form['upper_energy'])
-    if request.form.get('energy_coverage_scale', False):
-        options.energy_coverage_scale = "linear"
-    else:
-        options.energy_coverage_scale = "log"
-    options.energy_width = float(request.form['energy_width'])
-
-    try:
-        evaluation_response = request.form['evaluation-lib']
-    except:
-        print("Failed to access form evaluation-lib")
-        evaluation_response = None
-    if evaluation_response == 1 or evaluation_response is None:
-        options.evaluation = "endf8"
-    if evaluation_response == 2:
-        options.evaluation = "endf7-1"
-
-    try:
-        scored_response = request.form['scored-metric']
-    except:
-        print("Failed to access form scored-metric")
-        scored_response = None
-    if scored_response == 1 or scored_response is None:
-        options.scored_metric = "chi_squared"
-    if scored_response == 2:
-        options.evaluation = "relative_error"
-
-    try:
-        weighting_response = request.form['weighting-function']
-    except:
-        print("Failed to access form weighting-function")
-        weighting_response = None
-
-    if weighting_response == 1 or weighting_response is None:
-        options.weighting_function = None
-    if weighting_response == 2:
-        options.weighting_function = "maxwell-boltzmann-room-temp"
-    if weighting_response == 3:
-        options.weighting_function = "maxwell-boltzmann-320C"
-    if weighting_response == 4:
-        options.weighting_function = "watt"
-
     options.required_reaction_channels = []
     if request.form.get('p,el', False):
         options.required_reaction_channels += [(2, 'P,EL')]
@@ -199,6 +160,7 @@ def generate_protons():
 
     metrics = grade_many_isotopes(options)
     plot_script, plot_component = plot_grades(metrics, options)
+    options_text = options.gen_html_description()
     return render_for_particle(options.projectile, options, version,
                                text_report, plot_script, plot_component, ai_chat_history, options_text)
 
@@ -207,6 +169,8 @@ def generate_protons():
 def set_neutrons():
     options.set_neutrons()
     text_report = ""
+    plot_script, plot_component = plot_grades(metrics, options)
+    options_text = options.gen_html_description()
     return render_for_particle(options.projectile, options, version,
                                text_report, plot_script, plot_component, ai_chat_history, options_text)
 
@@ -214,6 +178,8 @@ def set_neutrons():
 def set_protons():
     options.set_protons()
     text_report = ""
+    plot_script, plot_component = plot_grades(metrics, options)
+    options_text = options.gen_html_description()
     return render_for_particle(options.projectile, options, version,
                                text_report, plot_script, plot_component, ai_chat_history, options_text)
 
@@ -226,6 +192,8 @@ def get_report():
         text_report = nuclide_metric.gen_report(options, for_web=True)
     else:
         text_report = "Nuclide not found."
+    plot_script, plot_component = plot_grades(metrics, options)
+    options_text = options.gen_html_description()
     return render_for_particle(options.projectile, options, version,
                                text_report, plot_script, plot_component, ai_chat_history, options_text)
 
